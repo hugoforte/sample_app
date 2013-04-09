d65491393bbd5450fc1cbadcddf63c89d101c035
Hugo Forte <hugo@fooda.com>
2013-04-09 11:38:24 -0500

    Updated to 4.0 / 2.0

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,13 +6,14 @@ describe "Static pages" do
 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      expect(page).to have_selector('h1', text: 'Sample App')
     end
-
+    
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Home",
+                        visible: false)
     end
   end
 
@@ -20,13 +21,14 @@ describe "Static pages" do
 
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      expect(page).to have_selector('h1', text: 'Help')
     end
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_selector('title',
+                        text: "Ruby on Rails Tutorial Sample App | Help",
+                        visible: false)
     end
   end
 
@@ -34,13 +36,14 @@ describe "Static pages" do
 
     it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
+      expect(page).to have_selector('h1', text: 'About Us')
     end
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | About Us",
+                    visible: false)
     end
   end
 end
